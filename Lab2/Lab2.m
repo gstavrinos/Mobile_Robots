009d69915ae3e335452c8a5bece5ad5f52f4382f
George Stavrinos <gstavrinos@iit.demokritos.gr>
2016-10-14 20:52:23 +0300

    Lab 2 ready

--- Lab2/Lab2.m
+++ Lab2/Lab2.m
@@ -106,8 +106,14 @@ disp ('------------')
 % %-----------<|
 
 %Using a for loop, we need to replicate the above commands
+%The commands below are the ones that we should run if the motor encoders
+%had 100% accuracy
 forward_commands = [2, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 4, 7];
 turning_commands = [90, -90, -90, 90, 90, -90, -90, 90, 90, -90, -90, -90];
+
+%By uncommenting the commands below, we get very close to the desired path
+%forward_commands = [1.8, 2.7, 0.7, 2.7, 0.65, 2.7, 0.65, 2.7, 0.65, 2.7, 0.65, 3.7, 6.8];
+%turning_commands = [84, -84, -84, 84, 84, -84, -84, 84, 84, -84, -85, -85];
 
 for i=1:13,
     travelDist(serPort, .5, forward_commands(i))
--- Lab2/lab2_viz.m
+++ Lab2/lab2_viz.m
@@ -1,5 +1,6 @@
 first_part = 'lab2_run';
 last_part = '.mat';
+visualize_test_path = true; %set to true to visualize the test run
 for i=1:5
     filename = strcat(strcat(first_part, num2str(i)),last_part);
     load(filename);
@@ -9,3 +10,13 @@ for i=1:5
     %plot(x,y)
     plot(x,y,'.')
 end
+
+if visualize_test_path
+    filename = 'lab2_test_run.mat';
+    load(filename);
+    x = cell2mat(datahistory(:,2));
+    y = cell2mat(datahistory(:,3));
+    figure
+    %plot(x,y)
+    plot(x,y,'.')
+end;
