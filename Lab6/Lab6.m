d1e06709e20afcfb005d7cd5837a10d8aebc58c0
George Stavrinos <gstavrinos@iit.demokritos.gr>
2016-11-09 21:26:15 +0200

    Code is ready. I now need to experiment with gains

--- Lab6/Lab6.m
+++ Lab6/Lab6.m
@@ -19,7 +19,7 @@ Kp = 100;
 Ki = 0.0001;
 Kd = 100;
     
-% Read the distance (odometry) sensor are initialize distance accumulator
+% Read the distance (odometry) sensor and initialize distance accumulator
 DistRead = DistanceSensorRoomba(serPort);
 Dist1 = 0;
 
@@ -34,9 +34,9 @@ LidarRes = LidarSensorCreate(serPort);
 [LidarM, LidarD] = min(LidarRes(341:681));
 
 
-wandering = 0;
+wandering = 1;
 homing = 0;
-following = 1;
+following = 0;
 distance_to_left = 0.5;
 distance_to_finish = 0.5;
 e100 = zeros(100,1);
@@ -46,15 +46,86 @@ t = 0;
 % to the wall in the desired range, and avoiding colliding with a wall both
 % on the side and in front.
 % Repeat for a given distance...
+
+disp('I dont know my initial position! Starting Task 1 - wandering');
+
 while(wandering || homing || following)
+    
     pause(.1)
 
     t = t + 1;
     % Read Left side of Lidar
     LidarRes = LidarSensorCreate(serPort);
     [LidarM, LidarD] = min(LidarRes(341:681));
+    [LidarM2, LidarD2] = min(LidarRes(1:341));
+
+    
+    %------------------------------------------------------------------------------
+    % Task-1: wander avoiding obstacles
+    if(wandering)
+        
+        if (LidarRes(341) < 1.0)
+            if ( LidarM < LidarM2) 
+                turnAngle(serPort, .2, -40);
+            else
+                turnAngle(serPort, .2, 40);
+            end
+        else
+            if ( LidarM < LidarM2 && LidarM < 0.5) 
+                turnAngle(serPort, .2, -10);
+            elseif (LidarM2 > LidarM2 && LidarM2 < 0.5)
+                turnAngle(serPort, .2, 10);
+            end
+        end
+
+        % Reset straight line and advance
+        SetDriveWheelsCreate(serPort, 0.5, 0.5);
+        
+        Camera = CameraSensorCreate(serPort);
+        if(any(Camera))
+           wandering = 0;
+           homing = 1;
+           following = 0;
+           disp('Saw beacon! Starting Task 2 - homing');
+        end
+    end
     
     
+    %------------------------------------------------------------------------------
+    % Task-2: homing into (reach) the beacon
+    if (homing)
+        %check front ~45 degrees
+        [LidarMmid, LidarDmid] = min(LidarRes(216:466));
+        Camera = CameraSensorCreate(serPort);
+        if (any(Camera) && abs(Camera) > 0.05 && LidarMmid >= 0.3)
+            turnAngle (serPort, .2, (Camera * 6));
+        else
+            if (LidarMmid < 0.3)
+                turnAngle (serPort, .2, (LidarDmid-341)/4);
+            elseif(~any(Camera))
+                wandering = 0;
+                homing = 0;
+                following = 1;
+                disp('Passed the beacon. Starting Task 3 - following');
+                Dist1 = 0;
+                lost_counter = 0;
+                %distance_to_finish = SonFF(idx1);
+                %distance_to_left = distance_to_finish;
+            end
+            
+        end
+        SetDriveWheelsCreate(serPort, 0.4, 0.4);
+        
+        
+    end
+    
+    
+    %------------------------------------------------------------------------------
+    % Task-3: check the sonar regularly while the robot moves, to keep the distance
+    % to the wall in the desired range, and avoiding colliding with a wall both
+    % on the side and in front. Repeat for a given distance...
+    
+    % Read the distance (odometry) sensor are initialize distance accumulator
     if (following)
         
         %are we there yet?!
@@ -62,30 +133,24 @@ while(wandering || homing || following)
            following = 0;
            wandering = 0;
            homing = 0;
+           disp('I think I reached my goal! :)');
            continue
         end
         
-        %Are we lost?! (checking if we are in the middle of the map, where no
-        %walls are visible from the sonars)
-%         if (SonFF(idx1) == 100 && SonRiF(idx1) == 100 && SonReF(idx1) == 100 && SonLF(idx1) == 100)
-%            lost_counter = lost_counter + 1;
-%            if(lost_counter > 5)
-%                wandering = 1;
-%                homing = 0;
-%                following = 0;
-%                continue
-%            end
-%         else
-%             %reset lost_counter. we need 5 consecutive "lost" conditions.
-%             lost_counter = 0;
-%         end
-
-        % Read Odometry sensor and accumulate distance measured.
-        DistRead = DistanceSensorRoomba(serPort);
-        Dist1 = Dist1 + DistRead;
-
-        % Reset straight line and advance
-        SetDriveWheelsCreate(serPort, 0.5, 0.5);
+        %Are we lost?! (checking left side is >= 1m for 5 consecutive measurements)
+        if (LidarM >= 1.0)
+           lost_counter = lost_counter + 1;
+           if(lost_counter > 5)
+               wandering = 1;
+               homing = 0;
+               following = 0;
+               disp('I am lost! Starting Task 1 - wandering');
+               continue
+           end
+        else
+            %reset lost_counter. we need 5 consecutive "lost" conditions.
+            lost_counter = 0;
+        end
         
         % Now do a simple porportional action, turning proportionally to how
         % far is from the set-point (0.5)
@@ -109,13 +174,15 @@ while(wandering || homing || following)
         O = P + I + D;
         
         turnAngle(serPort, .2, O);
-
+        
         % Read Odometry sensor and accumulate distance measured.
         DistRead = DistanceSensorRoomba(serPort);
         Dist1 = Dist1 + DistRead;
 
         % Reset straight line and advance
         SetDriveWheelsCreate(serPort, 0.5, 0.5);
+        %Give some extra time for the odom reading
+        pause(.1)
     end 
 end
 
