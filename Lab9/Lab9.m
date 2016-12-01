b4f6c89a2a6025fe89f1f02b1e78440205d0bde7
George Stavrinos <gstavrinos@iit.demokritos.gr>
2016-12-01 03:29:13 +0200

    Code is ready. Some more tests and we are done!

--- Lab9/Lab9.m
+++ Lab9/Lab9.m
@@ -13,22 +13,25 @@ function Lab9(serPort)
     % Let's define some connections on the map
     connections = { 'BA' 'RA';
                     'RA' 'BA';
-                    'GA' 'RA';
-                    'RA' 'GA';
-                    'BB' 'GA';
+                    'GA' 'RB';
+                    'GB' 'RC';
+                    'GC' 'RD';
+                    'GD' 'RE';
+                    'BB' 'RA';
                     'RB' 'BB';
                     'BG' 'RB';
                     'RC' 'BC';
                     'BC' 'RC';
                     'RD' 'BD';
-                    'BD' 'RC';
+                    'BD' 'RD';
                     'RE' 'BE';
                     'BE' 'RE';
                     'RE' 'BF';
                     'BF' 'RB';
                     'RB' 'GB';
+                    'RA' 'GA';
                     'RC' 'GC';
-                    'RD' 'GD'
+                    'RD' 'GD';
                     };
                 
     % Current estimated position
@@ -40,17 +43,24 @@ function Lab9(serPort)
     % Check if wandering is still trying to approach a wall
     wandering_init = 1;
     
+    % We will write ALL localization entries to a file, in order to
+    % evaluate the procedure easily
+    t = datetime('now','TimeZone','local','Format','d-MMM-y_HH_mm_ss');
+    folder = 'localization_sessions/';
+    file = fopen(strcat(folder,strcat(char(t),'_localization_session.txt')),'w');
+    
+    % Avoid spamming the session files with the same values
+    last_written_pos = 'NONE';
+    last_written_orient = 'NONE';
+    
+    % We will use a kind of memory, to compare the last estimation with the
+    % current estimation
     last_pos_estimation = 'UNKNOWN';
     last_orient_estimation = 'UNKNOWN';
     
-    % RED: B, C, D
-    % Total = 2
-    
-    % RED: A
-    % Total > 1.5 && Total < 2
-    
-    % RED: E
-    % Total = 3
+    % I am going to use this variable to detect the major region changes by
+    % detecting if the PID controller is sending a major turning command.
+    heavy_turning = 0;
 
     % Sets forward velocity using differential system
     SetDriveWheelsCreate(serPort, 0.5, 0.5);
@@ -61,10 +71,6 @@ function Lab9(serPort)
     Ki = 0.001;
     % Differential Gain
     Kd = 150;
-
-    % Read the distance (odometry) sensor and initialize distance accumulator
-    DistRead = DistanceSensorRoomba(serPort);
-    Dist1 = 0;
 
     % Read the back sonar so that we know when to stop!
     SonRead = ReadSonar(serPort, 4);
@@ -85,7 +91,7 @@ function Lab9(serPort)
     % Check front ~45 degrees
     [LidarMmid, LidarDmid] = min(LidarRes(216:466));
 
-    % This our plan. The sequence of tasks.
+    % This is our plan. The sequence of tasks.
     plan = ['w', 'h', 'f'];
     
     % Obstacle Avoidance is disabled for now
@@ -113,12 +119,6 @@ function Lab9(serPort)
         t = t + 1;
 
         LidarRes = LidarSensorCreate(serPort);
-        
-        % Read odometry sensor
-        % Give some extra time for the odom reading. Without this extra
-        % pause, the odometry reading does not seem to return anything!
-        pause(.1)
-        DistRead = DistanceSensorRoomba(serPort);
 
         % Read Left side of Lidar
         [LidarL, LidarD] = min(LidarRes(341:681));
@@ -168,6 +168,7 @@ function Lab9(serPort)
     % Stop motors
     SetDriveWheelsCreate(serPort, 0, 0);
     disp('I reached my goal! :)');
+    fclose(file);
 
     
     % ------------------------------------------------------------------
@@ -180,25 +181,14 @@ function Lab9(serPort)
     % since the robot does not know its initial position, so it should
     % start wandering until it finds the beacon.
     function robotWandering
-%         % If there is not enough space in front of the robot
-%         if (LidarRes(341) < 1.0)
-%             if ( LidarL < LidarR) 
-%                 turnAngle(serPort, .2, -40);
-%             else
-%                 turnAngle(serPort, .2, 40);
-%             end
-%         end
-%         % Check the sides using the LIDAR and try to stay away from walls
-%         if ( LidarL < LidarR && LidarL < 0.5) 
-%             turnAngle(serPort, .2, -10);
-%         elseif (LidarR > LidarR && LidarR < 0.5)
-%             turnAngle(serPort, .2, 10);
-%         end
-
+        
+        % We are now using the PID controller for wandering too.
+        % This makes wandering more time consuming, but enables us to
+        % localize faster!
         if ~wandering_init
             turnAngle(serPort, .2, PID);
         else
-            if LidarMmid <= 0.6
+            if LidarMmid <= 0.6 || approx(LidarL, 0.5)
                 wandering_init = 0;
             end
         end
@@ -217,19 +207,27 @@ function Lab9(serPort)
         end 
         % Reset straight line and advance
         SetDriveWheelsCreate(serPort, 0.4, 0.4);
+        localization
     end
 
     % Planned Task - Following the path
     function robotFollowing
 
         % Obey your PID controller make a turn that equals P + I + D
-        turnAngle(serPort, .2, PID);
-
-        % Accumulate distance measured.
-        Dist1 = Dist1 + DistRead;
+        command = PID;
+        
+        % Detect if we make a sharp turn left
+        if command >= 4
+           heavy_turning = 1;
+        else
+           heavy_turning = 0;
+        end
+        
+        turnAngle(serPort, .2, command);
 
         % Reset straight line and advance
         SetDriveWheelsCreate(serPort, 0.5, 0.5);
+        localization
     end
 
     % This is a method that manages the different planned tasks. This could
@@ -246,12 +244,12 @@ function Lab9(serPort)
             if(~any(Camera))
                 plan = 'f';
                 disp('Passed the beacon. Starting Task 3 - following');
-                Dist1 = 0;
                 lost_counter = 0;
             end
         elseif plan(1) == 'f'
             % Are we there yet?!
-            if (Dist1 >= 17 && SonReF(1) > 2.1 && SonReF(1) < 100)
+            % Instead of using odometry, I am using localization now!
+            if (strcmp(currPos, 'BE') && SonReF(1) > 2.1 && SonReF(1) < 100)
                plan = [];
             end
 
@@ -261,6 +259,8 @@ function Lab9(serPort)
                lost_counter = lost_counter + 1;
                if(lost_counter > 5)
                    plan = ['w', 'h', 'f'];
+                   currPos = 'UNKNOWN';
+                   currOrient = 'UNKNOWN';
                    disp('I am lost! Starting Task 1 - wandering');
                end
             else
@@ -281,9 +281,9 @@ function Lab9(serPort)
 
 
 
-    % The PID controller implementation
+    % The PID controller implementation, now in its own function!
     function O = PID
-        % Now do a simple porportional action. This part is used as was
+        % Now do a simple proportional action. This part is used as was
         % provided, but with a change on the variable names.
         et = LidarL - 0.5;
         P = Kp * et;
@@ -309,132 +309,163 @@ function Lab9(serPort)
 
     % A function that tries to imitate localization
     function localization
-%         % Read Left side of Lidar (216:466)
-%         [LidarL2, LidarD] = min(LidarRes(501:521));
-% 
-%         % Read Right side of Lidar
-%         [LidarR2, LidarD2] = min(LidarRes(161:181));
-
-     SonRead = ReadSonar(serPort, 1);
-        if ~any(SonRead) 
-            LidarR2(1) = 100;
-        else
-            LidarR2(1) = SonRead;
-        end
-
-        SonRead = ReadSonar(serPort, 2);
-        if ~any(SonRead) 
-            LidarMmid(1) = 100;
-        else
-            LidarMmid(1) = SonRead;
-        end
-
-        SonRead = ReadSonar(serPort, 3);
-        if ~any(SonRead)
-            LidarL2(1) = 100;
-        else
-            LidarL2(1) = SonRead;
-        end
-        
-        LidarL2
-        LidarR2
-        
-        estimatedPos = {'UNKNOWN'};
-        estimatedOrient = {'UNKNOWN'};
-        blue_chaos = 0;
-        if approx(LidarL2, 0.5) && LidarMmid >= 0.7
-            % Check if we are in a Blue region
-            if LidarR2 > 3
-               estimatedPos = {'BA' 'BB' 'BC' 'BD' 'BE' 'BF'};
-               estimatedOrient = {'U' 'D' 'L' 'L' 'L' 'U'};
-               blue_chaos = 1;
-            % Check if we are in Red E
-            elseif approx(LidarR2, 2.5)
-                estimatedPos = {'RE'};
-                if LidarMmid <= 3 && LidarR2 > 1.2
-                    estimatedOrient = {'D'};
-                else
-                    estimatedOrient = {'U'}; 
-                end
-            % Check if we are in one of the rest Red regions
-            elseif approx(LidarR2, 1.5)
-                if LidarMmid > 3
-                %TODO I can't think of an orientation characteristic, yet!
-                   % TODO I need a way to distinguish the two Bs and two As!
-                   estimatedPos = {'RA' 'RB' 'RC' 'RD'}; 
-                   estimatedOrient = {'UNKNOWN' 'U' 'U'};
-                else
-                    disp('RC/RD')
-                   estimatedPos = {'RC' 'RD'}; 
-                   estimatedOrient = {'D' 'D'};
-                end
+        if plan(1) ~= 'h'
+            % Use the sonars for localization
+            SonRead = ReadSonar(serPort, 1);
+            if ~any(SonRead) 
+                SonarR(1) = 100;
+            else
+                SonarR(1) = SonRead;
             end
-        end
-        filtered_pos_estimations = {};
-        filtered_orient_estimations = {};
-        if ~strcmp(currPos,'UNKNOWN')
-            for i=1:numel(estimatedPos)
-                for j=1:size(connections,1)
-                   % With this OR we make sure that each region
-                   % connects with itself, and regions are interconnected!
-                   if strcmp(connections(j,2),estimatedPos(i)) && strcmp(connections(j,1),currPos)
-                        filtered_pos_estimations(end+1) = estimatedPos(i);
-                        filtered_orient_estimations(end+1) = estimatedOrient(i);
-                   end
-                end
+
+            SonRead = ReadSonar(serPort, 2);
+            if ~any(SonRead) 
+                SonarF(1) = 100;
+            else
+                SonarF(1) = SonRead;
             end
-            if numel(filtered_pos_estimations) > 0
-                disp('found path!')
-                filtered_pos_estimations(1)
-                if numel(filtered_pos_estimations) == 1
-                    currPos = filtered_pos_estimations(1);
-                    currOrient = filtered_orient_estimations(1);
-                else
-                    for i=1:numel(filtered_pos_estimations)
-                        % Check some multiple paths that change based on
-                        % orientation only
-                       if ((strcmp(currPos,'RE') && strcmp(currOrient,'D') && strcmp(filtered_pos_estimations(i),'BE'))||(strcmp(currPos,'RE') && strcmp(currOrient,'U') && strcmp(filtered_pos_estimations(i),'BF')))
-                            currPos = filtered_pos_estimations(i);
-                            currOrient = filtered_orient_estimations(i);
-                            break;
+
+            SonRead = ReadSonar(serPort, 3);
+            if ~any(SonRead)
+                SonarL(1) = 100;
+            else
+                SonarL(1) = SonRead;
+            end
+
+            % Check if the robot is currently in any of the red areas except E
+            in_red_region = 0;
+            if strcmp(currPos,'RB') || strcmp(currPos,'RC') || strcmp(currPos,'RD') || strcmp(currPos,'RA') || strcmp(currPos,'BC') || strcmp(currPos,'BD')
+               in_red_region = 1; 
+            end
+
+            estimatedPos = {'UNKNOWN'};
+            estimatedOrient = {'UNKNOWN'};
+            blue_chaos = 0;
+            if ~(heavy_turning && in_red_region)
+                if approx(SonarL, 0.3) && SonarF >= 0.7
+                    % Check if we are in a Blue region
+                    if SonarR > 3
+                       estimatedPos = {'BA' 'BB' 'BC' 'BD' 'BE' 'BF'};
+                       estimatedOrient = {'U' 'D' 'L' 'L' 'L' 'U'};
+                       blue_chaos = 1;
+                    % Check if we are in Red E
+                    elseif approx(SonarR, 2.3)
+                        estimatedPos = {'RE'};
+                        if SonarF <= 3 && SonarR > 1.2
+                            estimatedOrient = {'D'};
+                        else
+                            estimatedOrient = {'U'}; 
+                        end
+                    % Check if we are in one of the other Red regions
+                    elseif approx(SonarR, 1.3)
+                        if SonarF > 3
+                           estimatedPos = {'RA' 'RB' 'RC' 'RD'}; 
+                           estimatedOrient = {'UNKNOWN' 'UNKNOWN' 'U' 'U'};
+                        else
+                           estimatedPos = {'RC' 'RD'}; 
+                           estimatedOrient = {'D' 'D'};
+                        end
+                    end
+                end
+            else
+               % Come here if we are in a red region and we are turning sharply
+               if strcmp(currPos,'RB')
+                   estimatedPos = {'GB'};
+                   estimatedOrient = {'L'};
+               elseif strcmp(currPos,'RC') || strcmp(currPos,'BC')
+                   estimatedPos = {'GC'};
+                   estimatedOrient = {'L'};
+               elseif strcmp(currPos,'RD') || strcmp(currPos,'BD')
+                   estimatedPos = {'GD'};
+                   estimatedOrient = {'L'};
+               elseif strcmp(currPos,'RA')
+                   estimatedPos = {'GA'};
+                   estimatedOrient = {'U'};
+               end
+            end
+            filtered_pos_estimations = {};
+            filtered_orient_estimations = {};
+            if ~strcmp(currPos,'UNKNOWN')
+                for i=1:numel(estimatedPos)
+                    for j=1:size(connections,1)
+                       % With this OR we make sure that each region
+                       % connects with itself, and regions are interconnected!
+                       if strcmp(connections(j,2),estimatedPos(i)) && strcmp(connections(j,1),currPos)
+                            filtered_pos_estimations(end+1) = estimatedPos(i);
+                            filtered_orient_estimations(end+1) = estimatedOrient(i);
                        end
                     end
                 end
-                if ~strcmp(currPos,'UNKNOWN')
-                    last_pos_estimation = currPos;
-                    last_orient_estimation = currOrient;
+                if numel(filtered_pos_estimations) > 0
+                    filtered_pos_estimations(1)
+                    if numel(filtered_pos_estimations) == 1
+                        currPos = filtered_pos_estimations(1);
+                        currOrient = filtered_orient_estimations(1);
+                    else
+                        for i=1:numel(filtered_pos_estimations)
+                            % Check some multiple paths that change based on
+                            % orientation only
+                           if ((strcmp(currPos,'RE') && strcmp(currOrient,'D') && strcmp(filtered_pos_estimations(i),'BE'))||(strcmp(currPos,'RE') && strcmp(currOrient,'U') && strcmp(filtered_pos_estimations(i),'BF')))
+                                currPos = filtered_pos_estimations(i);
+                                currOrient = filtered_orient_estimations(i);
+                                break;
+                           end
+                        end
+                    end
+                    if ~strcmp(currPos,'UNKNOWN')
+                        last_pos_estimation = currPos;
+                        last_orient_estimation = currOrient;
+                    end
+
+                elseif numel(estimatedPos) == 1
+                    if strcmp(estimatedPos(1),last_pos_estimation) && ~strcmp(estimatedPos(i),'UNKNOWN')
+                        currPos = estimatedPos(1);
+                        currOrient = estimatedOrient(1);
+                    else
+                        if ~strcmp(estimatedPos(i),'UNKNOWN')
+                            last_pos_estimation = estimatedPos(1);
+                            last_orient_estimation = estimatedOrient(1);
+                        end
+                    end
                 end
-                
-            elseif numel(estimatedPos) == 1
-                if strcmp(estimatedPos(1),last_pos_estimation)
+            else
+                if numel(estimatedPos) == 1 && ~strcmp(estimatedPos(1),'UNKNOWN')
                     currPos = estimatedPos(1);
                     currOrient = estimatedOrient(1);
-                else
-                    if ~strcmp(estimatedPos(i),'UNKNOWN')
-                        last_pos_estimation = estimatedPos(1);
-                        last_orient_estimation = estimatedOrient(1);
+                    last_pos_estimation = estimatedPos(1);
+                    last_orient_estimation = estimatedOrient(1);
+                elseif blue_chaos
+                    if ~strcmp(last_written_pos, 'I know that I am in a blue region, but I am not sure in which one!\n')
+                        fprintf(file,strcat('Current Task: ', plan(1)));
+                        fprintf(file,'\n');
+                        fprintf(file,'I know that I am in a blue region, but I am not sure in which one!\n');
+                        fprintf(file,'----------------------------\n');
+                        last_written_pos = 'I know that I am in a blue region, but I am not sure in which one!\n';
+                        last_written_orient = 'NONE';
                     end
                 end
             end
         else
-            if numel(estimatedPos) == 1 && ~strcmp(estimatedPos(1),'UNKNOWN')
-                disp('I used to be lost, man...')
-                currPos = estimatedPos(1);
-                currOrient = estimatedOrient(1);
-                last_pos_estimation = estimatedPos(1);
-                last_orient_estimation = estimatedOrient(1);
-            elseif blue_chaos
-                disp('I know that I am in a blue region, but in which?')
-            end
+            currPos = 'RA';
+            currOrient = 'UNKNOWN';
         end
-        currPos
-        currOrient
+        if ~(strcmp(last_written_pos, currPos) && strcmp(last_written_orient, currOrient))
+            fprintf(file,strcat('Current Task: ', plan(1)));
+            fprintf(file,'\n');
+            fprintf(file,strcat('Current Position: ', char(currPos)));
+            fprintf(file,'\n');
+            fprintf(file,strcat('Current Orientation: ', char(currOrient)));
+            fprintf(file,'\n');
+            fprintf(file,'----------------------------\n');
+            last_written_pos = currPos;
+            last_written_orient = currOrient;
+        end
     end
 
     % A helper function to approximate values
     function result = approx(number1, number2)
         result = 0;
-        if abs(number1 - number2) <= 0.2
+        if abs(number1 - number2) <= 0.15
             result = 1;
         end
     end
