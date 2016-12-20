3d4b1284b62db783e1fe50ddfef11ff0845c8a8b
George Stavrinos <gstavrinos@iit.demokritos.gr>
2016-12-20 23:56:22 +0200

    We are now able to getout of the door! The code needs some serious cleaning though...

--- final_project/final_project.m
+++ final_project/final_project.m
@@ -50,10 +50,13 @@ function final_project(serPort)
     % measure the right side.
     LidarRes = LidarSensorCreate(serPort);
     % Read Left side of Lidar
-    [LidarL, LidarD] = min(LidarRes(341:681));
+    [LidarLall, LidarD] = min(LidarRes(341:681));
+    [LidarL, LidarD] = min(LidarRes(441:681));  % use only the sides
 
     % Read Right side of Lidar
-    [LidarR, LidarD2] = min(LidarRes(1:341));
+    [LidarRall, LidarD2] = min(LidarRes(1:341));
+    [LidarR, LidarD2] = min(LidarRes(1:241)); % use only the sides
+
 
 
     % Check front ~45 degrees
@@ -61,7 +64,7 @@ function final_project(serPort)
 
     % The possible tasks are coded like this:
     % 0 = find the center of the starting room
-    % 1 = find the exit of the starting room
+    % 1 = face the exit of the starting room
     % 2 = exit the starting room
     % 3 = find beacon
     % 4 = approach beacon
@@ -100,11 +103,6 @@ function final_project(serPort)
         t = t + 1;
 
         LidarRes = LidarSensorCreate(serPort);
-        
-        % Read odometry sensor
-        % Give some extra time for the odom reading. Without this extra
-        % pause, the odometry reading does not seem to return anything!
-        pause(.1)
 
         % Read Left side of Lidar
         [LidarL, LidarD] = min(LidarRes(341:681));
@@ -150,19 +148,22 @@ function final_project(serPort)
         % reactive task in this loop
         %if ~obstacle_avoidance
             % Change the plan based on our new environment
-            %planManager
+            planManager
             
             % Run the corresponding planned task
             if size(plan) > 0
-                % Task-1: Wandering mode
-                if plan(1) == 0
-                    findCenter
-                % Task-2: Homing mode
-                %elseif plan(1) == 'h'
-                %    robotHoming
-                % Task-3: Following mode
-                %elseif plan(1) == 'f'
-                %    robotFollowing
+                % Task-1: Centering in starting room
+                if plan(1) == 0 || plan(1) == 2
+        
+                    % Override the PID controller, to make the process faster, and
+                    % safer!
+                    if SonFF < 0.5
+                        turnAngle(serPort, .2, 70);
+                        t = 1;
+                    end
+                    PID_controller(LidarL/4, LidarR/4)
+                elseif plan(1) == 1
+                    door_alignment
                 end
             end
         %end
@@ -177,17 +178,10 @@ function final_project(serPort)
     
     
     % Helper functions
-    function findCenter
-        
-        % Override the PID controller, to make the process faster, and
-        % safer!
-        if SonFF < 0.3
-            turnAngle(serPort, .2, 90);
-        end
-        
+    function PID_controller(value1, value2)
         % Now do a simple porportional action. This part is used as was
         % provided, but with a change on the variable names.
-        et = LidarL - LidarR;
+        et = value1 - value2;
         P = Kp * et;
         if (P > 50) 
             P = 50;
@@ -216,47 +210,57 @@ function final_project(serPort)
         
         % This should go inside the plan manager, but let's test it here
         % for now!
-        if exiting
-            if SonRiF == 100 || SonLF == 100 % The robot is facing down or up!
-                if approx(SonReF,SonFF)
-                   % I should change the plan here in the future!
-                   disp('We are in the center of the room!') 
-                end
-            elseif SonFF == 100 || SonReF == 100 % The robot is facing left or right!
-                if approx(SonRiF,SonLF)
-                   % I should change the plan here in the future!
-                   disp('We are in the center of the room!')
-                end
-            end
-        end
-        
+    end
+
+    function door_alignment
+       if exiting
+           if SonFF < 100
+              turnAngle(serPort, .2, 10); 
+           end
+       end
     end
 
     % This is a method that manages the different planned tasks. This could
     % be described as a reactive task, although it is related to the
     % requirements of the plan.
-%     function planManager
-%         if plan(1) == 'w'
-%             % Check the camera for the beacon
+    function planManager
+        if plan(1) == 0
+            if exiting
+                if SonRiF == 100 || SonLF == 100 % The robot is facing down or up!
+                    if approx(SonReF,SonFF,.1) && (approx(SonRiF,1.5,.2) || approx(SonLF,1.5,.2))
+                       plan = [1, 2, 3, 4, 5, 6];
+                        disp('CENTEEEEER');
+                    end
+                elseif SonFF == 100 || SonReF == 100 % The robot is facing left or right!
+                    if approx(SonRiF,SonLF,.1) && (approx(SonFF,2,.2) || approx(SonFF,2,.2))
+                       plan = [1, 2, 3, 4, 5, 6];
+                        disp('CENTEEEEER');
+                    end
+                elseif approx(SonFF,SonReF,.1) && approx(SonRiF,SonLF,.1)
+                    plan = [1, 2, 3, 4, 5, 6];
+                    disp('CENTEEEEER');
+                end
+            end
+            
+            % Check the camera for the beacon
 %             if(any(Camera))
 %                plan = ['h', 'f'];
 %                disp('Saw beacon! Starting Task 2 - homing');
 %             end
-%         elseif plan(1) == 'h'
-%             if(~any(Camera))
-%                 plan = 'f';
-%                 disp('Passed the beacon. Starting Task 3 - following');
-%                 Dist1 = 0;
-%                 lost_counter = 0;
-%             end
-%         elseif plan(1) == 'f'
-%             % Are we there yet?!
+        elseif plan(1) == 1 && SonFF == 100
+            disp('Aligned with door!');
+            plan = [2, 3, 4, 5, 6];
+        elseif plan(1) == 2 && approx(LidarMmid,3.5,.1)
+            disp('I am out of the door!');
+            plan = [3, 4, 5, 6];
+            
+            % Are we there yet?!
 %             if (Dist1 >= 17 && SonReF(1) > 2.1 && SonReF(1) < 100)
 %                plan = [];
 %             end
-% 
-%             % Are we lost?! (checking if left side is >= 1m for 5 
-%             % consecutive measurements)
+
+            % Are we lost?! (checking if left side is >= 1m for 5 
+            % consecutive measurements)
 %             if (LidarM >= 1.0)
 %                lost_counter = lost_counter + 1;
 %                if(lost_counter > 5)
@@ -267,13 +271,13 @@ function final_project(serPort)
 %                 % Reset lost_counter. We need 5 consecutive "lost" conditions.
 %                 lost_counter = 0;
 %             end
-%         end
-%     end
+        end
+    end
 
 % A helper function to approximate values
-    function result = approx(number1, number2)
+    function result = approx(number1, number2, limit)
         result = 0;
-        if abs(number1 - number2) <= 0.1
+        if abs(number1 - number2) <= limit
             result = 1;
         end
 end
